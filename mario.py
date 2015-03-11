91273e19a363fb9b0c28b6c338e559442bff41db
ChevreSolitaire <a.ferail@laposte.net>
2015-03-11 01:34:27 +0100

    This is it

--- mario.py
+++ mario.py
@@ -11,19 +11,27 @@ from classes_foncts import *
 
 # Sprites
 marioSprite = SpriteImage("images/mario sheet.png", vertFond, 1)
+goombaSprite = SpriteImage("images/goomba sheet.png", blancFond, 1)
 marioStand = marioSprite.get_imageXY(72, 5, 87, 31)
+monstresStand = goombaSprite.get_imageXY(72, 5, 87, 31)
 mario = Mario(marioStand)
+monstres = Monstres(monstresStand)
 
 gameOverSprite = SpriteImage("images/game over.png", noirFond, 0)
 gameOver = gameOverSprite.get_imageXY(5, 7, 260, 230)
 
 active_sprite_list = pygame.sprite.Group()
 active_sprite_list.add(mario)
+active_sprite_list.add(monstres)
 
 while continuer:
     for event in pygame.event.get():
         if event.type == QUIT:
             continuer = 0
+        if mario.yolo == 1:
+            active_sprite_list.remove(monstres)
+            active_sprite_list.update()
+            mario.yolo = 0
         if menu:
             screen.blit(menuImage, (0, 0))
             menuCurseurPos = choixMenu(event, menuCurseurPos)
@@ -67,7 +75,7 @@ while continuer:
                 menu, levelSelection, jeu, levelCurrent = 0, 0, 1, levelCurseurPos
         elif jeu:
             jeuFonct(event, mario)
-    # Niveaux
+
     if jeu:
         choix = "n" + str(levelCurrent + 1) + ".txt"
         if generation_level:
@@ -79,11 +87,19 @@ while continuer:
             niveau.afficher(screen)
             generation_level = 0
 
-        screen.blit(bg_list[levelCurrent], (0, 0))
-        block_list.update()
-        block_list.draw(screen)
-        active_sprite_list.update()
-        active_sprite_list.draw(screen)
+        if ((pygame.sprite.collide_rect(mario, monstres)) and willDie == 1) or (mario.rect.y >= screenY) :
+            screen.blit(bg_list[levelCurrent], (0, 0))
+            block_list.draw(screen)
+            active_sprite_list.draw(screen)
+            mario.death()
+
+        if mario.time == 210:
+            screen.blit(bg_list[levelCurrent], (0, 0))
+            block_list.update()
+            block_list.draw(screen)
+            monstresMovement(monstres)
+            active_sprite_list.update()
+            active_sprite_list.draw(screen)
 
         if (event.type == KEYDOWN and event.key == K_ESCAPE) or mario.reset == 1:
             block_list.empty()
@@ -91,10 +107,13 @@ while continuer:
             screen.blit(menuCurseurImage, levelCurseurList[levelCurseurPos])
             music_menu(volume_default)
             mario.rect.x, mario.rect.y = 0, 0
+            monstres.rect.x, monstres.rect.y = 100, 300
             jeu, levelSelection, levelCurrent, generation_level = 0, 1, -1, 1
             pygame.key.set_repeat(0, 0)
             mario.reset = 0
             mario.time = 210
+            monstres = Monstres(monstresStand)
+            active_sprite_list.add(monstres)
 
     clock.tick(60)
     pygame.display.flip()
